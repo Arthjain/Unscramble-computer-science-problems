9895dc725fc6c72b9e0a72ff4fba167ad0ea9785
Arth jain <arthjainabc@gmail.com>
2020-06-02 00:09:47 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,36 +1,23 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files.
-"""
 import csv
-
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
+from datetime import datetime
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-"""
-TASK 1:
-How many different telephone numbers are there in the records? 
-Print a message:
-"There are <count> different telephone numbers in the records."
-"""
-
-text_out_set = set([text[0] for text in texts])
-text_in_set = set([text[1] for text in texts])
-
-call_out_set = set([call[0] for call in calls])
-call_in_set = set([call[1] for call in calls])
-
-different_records = set()
-
-different_records.update(text_out_set)
-different_records.update(text_in_set)
-
-different_records.update(call_out_set)
-different_records.update(call_in_set)
-
-print("There are {} different telephone numbers in the records.".format(len(different_records)))
+def Recordofparticulardate(calls):
+    new_lists = {}
+    for item in calls:
+        if item[2][3:10] == '09-2016':
+            if item[0] not in new_lists:
+                new_lists[item[0]] = int(item[3])
+            else:
+                new_lists[item[0]] += int(item[3])
+            if item[1] not in new_lists:
+                new_lists[item[1]] = int(item[3])
+            else:
+                new_lists[item[1]] += int(item[3])
+        
+    return new_lists
+phoneno = Recordofparticulardate(calls)
+max_in= max(phoneno, key=phoneno.get)
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(max_in,phoneno[max_in]))
