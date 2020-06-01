01c7f94a660b423475c54d11d0703c4620f5401e
Arth jain <arthjainabc@gmail.com>
2020-06-01 22:39:19 +0530

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,5 +1,9 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
 import csv
-from itertools import chain
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -7,22 +11,29 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+marketing_numbers = set()
+non_marketing_numbers = set()
 
+for row in texts:
+    non_marketing_numbers.add(row[0])
+    non_marketing_numbers.add(row[1])
+for row in calls:
+    non_marketing_numbers.add(row[1])
+    marketing_numbers.add(row[0])
 
-telephone_numbers_in_texts = list(chain.from_iterable([(sender, reciever) for sender, reciever, _ in texts]))
-texters = set(telephone_numbers_in_texts)
-
-callers = set()
-call_recievers = set()
-
-for caller, reciever, _, _ in calls:
-    callers.add(caller)
-    call_recievers.add(reciever)
-
-# telemarkerters don't text or revicieve callers
-possible_telemarkerters = callers - (texters | call_recievers)
-
-print("These numbers could be telemarketers:")
-
-for tel_number in sorted(possible_telemarkerters):
-    print(tel_number)
+    
+marketing_numbers -= marketing_numbers & non_marketing_numbers
+marketing_numbers = sorted(marketing_numbers)
+print("These numbers could be telemarketers: ")
+print('\n'.join(marketing_numbers)) 
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers:
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+Print a message:
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
