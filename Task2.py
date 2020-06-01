022a25ee7f332b77968c2c9b8b902c6db60496d4
Arth jain <arthjainabc@gmail.com>
2020-06-01 17:00:43 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,7 +1,3 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files
-"""
 from itertools import chain
 from collections import deque, defaultdict
 from datetime import datetime
@@ -13,14 +9,6 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-"""
-TASK 2: Which telephone number spent the longest time on the phone
-during the period? Don't forget that time spent answering a call is
-also time spent on the phone.
-Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
-September 2016.".
-"""
 
 calls_dictionary = defaultdict(int)
 
