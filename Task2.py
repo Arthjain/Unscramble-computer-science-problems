57a2349a24df83b5efe7b32cacd5c393eb43e4ec
Arth jain <arthjainabc@gmail.com>
2020-06-01 22:37:07 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,7 +1,9 @@
-from itertools import chain
-from collections import deque, defaultdict
-from datetime import datetime
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
 import csv
+from datetime import datetime
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -10,16 +12,31 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-calls_dictionary = defaultdict(int)
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
 
-for caller, reciever, timestamp, duration in calls:
-    date = datetime.strptime(timestamp, "%d-%m-%Y %H:%M:%S")
-    if date.year == 2016 and date.month == 9:
-        calls_dictionary[caller] += int(duration)
-        calls_dictionary[reciever] += int(duration)
-
-template = "{} spent the longest time, {} seconds, on the phone during September 2016."
-
-
-highest_duration = max(calls_dictionary.items(), key=lambda x: x[1])
-print(template.format(*highest_duration))
+def GetRecordsbyDate(calls):
+    Lists = {}
+    for item in calls:
+        #print(item[2][3:10])
+        if item[2][3:10] == '09-2016':
+            if item[0] not in Lists:
+                Lists[item[0]] = int(item[3])
+            else:
+                Lists[item[0]] += int(item[3])
+            if item[1] not in Lists:
+                Lists[item[1]] = int(item[3])
+            else:
+                Lists[item[1]] += int(item[3])
+        
+    return Lists
+phoneNumbers = GetRecordsbyDate(calls)
+max_key = max(phoneNumbers, key=phoneNumbers.get)
+#print(max_value,max_key)
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(max_key,phoneNumbers[max_key]))
