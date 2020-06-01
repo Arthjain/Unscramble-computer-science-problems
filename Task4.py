85319f01566f14b0af14c705399cd90dd7c0aed6
Arth jain <arthjainabc@gmail.com>
2020-06-01 17:03:17 +0530

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,7 +1,3 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files.
-"""
 import csv
 from itertools import chain
 with open('texts.csv', 'r') as f:
@@ -12,18 +8,6 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-"""
-TASK 4:
-The telephone company want to identify numbers that might be doing
-telephone marketing. Create a set of possible telemarketers:
-these are numbers that make outgoing calls but never send texts,
-receive texts or receive incoming calls.
-
-Print a message:
-"These numbers could be telemarketers: "
-<list of numbers>
-The list of numbers should be print out one per line in lexicographic order with no duplicates.
-"""
 
 telephone_numbers_in_texts = list(chain.from_iterable([(sender, reciever) for sender, reciever, _ in texts]))
 texters = set(telephone_numbers_in_texts)
