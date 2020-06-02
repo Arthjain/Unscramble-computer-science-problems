bbb213a741abc69a4ee0ccfa5cb868f4c0c74089
Arth jain <arthjainabc@gmail.com>
2020-06-02 09:21:01 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -3,16 +3,16 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-new_lists = {}
+dict_calllogs = {}
 for item in calls:
     if item[2][3:10] == '09-2016':
-        if item[0] not in new_lists:
-            new_lists[item[0]] = int(item[3])
+        if item[0] not in dict_calllogs:
+            dict_calllogs[item[0]] = int(item[3])
         else:
-            new_lists[item[0]] += int(item[3])
-        if item[1] not in new_lists:
-            new_lists[item[1]] = int(item[3])
+            dict_calllogs[item[0]] += int(item[3])
+        if item[1] not in dict_calllogs:
+            dict_calllogs[item[1]] = int(item[3])
         else:
-            new_lists[item[1]] += int(item[3])
-max_in= max(new_lists, key=new_lists.get)
-print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(max_in,new_lists[max_in]))
+            dict_calllogs[item[1]] += int(item[3])
+max_in= max(dict_calllogs , key=dict_calllogs.get)
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(max_in,dict_calllogs[max_in]))
