02086965ff1eabea1a63738fcc2b67e03b019a44
Arth jain <arthjainabc@gmail.com>
2020-06-02 09:38:24 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -2,21 +2,21 @@ import csv
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-numbers_called = []
+fixed_lines_calls = []
 for n in range(len(calls)):
-    if calls[n][0][:5] == '(080)':
-        if calls[n][1][0] == '(':
+    if '(080)' in calls[n][0][:5]:
+        if '140' in calls[n][1][:3]:
+            fixed_lines_calls.append('140')
+        elif '(' in calls[n][1][0]:
             par_index = calls[n][1].find(')')
-            numbers_called.append(calls[n][1][:par_index+1])
-        elif calls[n][1][:3] == '140':
-            numbers_called.append('140')
+            fixed_lines_calls.append(calls[n][1][:par_index+1])
         else:
-            numbers_called.append(calls[n][1][:4])
-l = len(numbers_called)
-count = numbers_called.count('(080)')
-numbers_called = sorted(set(numbers_called))
+            fixed_lines_calls.append(calls[n][1][:4])
+l = len(fixed_lines_calls)
+count = fixed_lines_calls.count('(080)')
+total_calls = sorted(set(fixed_lines_calls))
 print("The numbers called by people in Bangalore have codes:")
-for n in numbers_called:
-    print(n)
+for i in total_calls:
+    print(i)
 percent = count * 100 / l
-print("{} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.".format(round(percent, 2)))
+print("{:.2f} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.".format(percent))
